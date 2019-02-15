406de5536143a538eb2f8152a572da6d21560e46
zintrulcre <zintrulcre@gmail.com>
2019-02-15 22:30:46 +1100

    v1.0.9
    Automatically pip3 install scrapy

--- LeetCode_Crawler/main.py
+++ LeetCode_Crawler/main.py
@@ -1,7 +1,8 @@
+import os
 import time
 from scrapy import cmdline
 
+os.system("pip3 install scrapy")
 start = time.process_time()
 cmdline.execute("scrapy crawl QuestionSetSpider".split())
 end = time.process_time()
-print("Total time: " + str(end - start))
