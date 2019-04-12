1b70e9b7fdc991a891b4ddf259be50fc5e1e5280
zintrulcre <zintrulcre@gmail.com>
2019-04-12 11:33:34 +1000

    installation sequence

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 import time
-from scrapy import cmdline
+
+start = time.time()
 
 os.system('sh ./requirements.sh')
 if os.path.exists("./.log"):
     os.remove("./.log")
+
+from scrapy import cmdline
 cmdline.execute("scrapy crawl QuestionSetSpider".split())
+
+end = time.time()
+print("Total time: " + str(end - start))
